2e55b5469e60410dfd45ba2e70b554769dd499b9
sathish005005 <106943294+sathish005005@users.noreply.github.com>
2022-08-02 16:31:27 +0530

    Add files via upload

---
+++ Calender/index.js
@@ -0,0 +1,8 @@
+dycalendar.draw({
+     target:'#dycalendar',
+     type: 'month',
+     dayformate:'full',
+     monthformat:'full',
+     highlighttargetdate: true,
+     prevnextbutton:'show'
+})
